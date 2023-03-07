4728ba3d6147e7127eae3168234568d17a08016f
casecam <109696610+casecam@users.noreply.github.com>
2023-03-07 14:34:18 -0800

    better refactor

--- src/app/utils/helpers/generics.ts
+++ src/app/utils/helpers/generics.ts
@@ -1,13 +1,25 @@
 import { NextFunction } from 'express';
 import { Model } from 'mongoose'
+import { Types } from 'mongoose'
 
-export const checkForNameErrors = async <T>(
+export const checkIfNameExists = async <T>(
   model: Model<T>,
+  _id: Types.ObjectId | string,
   name: string,
+  method: string,
   next: NextFunction
-): Promise<boolean | void> => {
-  const result = await model.find({ name })
-  if (result.length) {
+): Promise<boolean | void> => {  
+  let exists = false
+  const results = await model.find({ name })
+
+  if (method === 'POST') {
+    if (results.length) exists = true
+  } else {
+    const sameNameFound = results.filter(item => item.id !== _id)
+    if (sameNameFound.length) exists = true
+  }
+  
+  if (exists) {
     return next(new Error(`${name} already exists`))
   } else {
     return false
